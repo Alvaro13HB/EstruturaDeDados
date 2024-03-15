910178b947b71dfa9894eb6b3ced8516c75d7566
Álvaro Becati <161021263+Alvaro13HB@users.noreply.github.com>
2024-03-15 15:32:06 -0300

    Update jogodavelha.cpp

--- jogodavelha.cpp
+++ jogodavelha.cpp
@@ -10,14 +10,18 @@ void createMatrix(char matrix[3][3]){
         }
     }
 }
-bool isPossible(char posicion, int linha, int coluna){
-    if((posicion == '.') && (linha > 0 && linha < 4) && (coluna > 0 && coluna < 4)){
+
+
+bool isPossible(char table[3][3], int linha, int coluna){
+    if((table[linha][coluna] == '.') && (linha >= 0 && linha < 3) && (coluna >= 0 && coluna < 3)){
         return true;
     }
     else{
         return false;
     }
 }
+
+
 void printMatrix(char matrix[3][3]){
     int i=0, j;
     cout << "1 2 3 \n";
@@ -29,6 +33,8 @@ void printMatrix(char matrix[3][3]){
     }
     cout << endl;
 }
+
+
 bool checkWinner(char matrix[3][3], int linha, int coluna, char symbol){
     int i, j, counting = 0;
     for(i = 0; i < 3; i++){
@@ -71,43 +77,53 @@ bool checkWinner(char matrix[3][3], int linha, int coluna, char symbol){
     return false;
 }
 
+
 int main(){
     char table[3][3], playersIcon[2] = {'X', 'O'}, replay = 'y';
-    int play = 0, l, c, player = 0;
-    bool win = false;
-
-    createMatrix(table);
+    int play, l, c, player;
+    bool ending = false;
     do{
-        printMatrix(table);
-        cout << "Jogada " << play + 1 << ": \n";
-        cout << "Digite sua jogada: ";
-        cin >> l >> c;
-        if(isPossible(table[l][c], l, c)){
-            table[l-1][c-1] = playersIcon[player];
-            play++;
-            if(player == 0){
-                player = 1;
+        player = 0;
+        play = 0;
+        createMatrix(table);
+        do{
+
+            printMatrix(table);
+            cout << "Jogada " << play + 1 << ": \n";
+            cout << "Digite sua jogada: ";
+            cin >> l >> c;
+            if(isPossible(table, l-1, c-1)){
+                table[l-1][c-1] = playersIcon[player];
+                play++;
+                if(player == 0){
+                    player = 1;
+                }
+                else{
+                    player = 0;
+                }
+
             }
             else{
-                player = 0;
+                cout << endl << "Jogada inválida, tente novamente!\n" << endl;
             }
 
-        }
-        else{
-            cout << endl << "Jogada inválida, tente novamente!\n" << endl;
-        }
-        if(checkWinner(table, l, c, playersIcon[player])){
-            cout << "O jogador " << playersIcon[player] << " venceu! Parabéns!\n"; 
-            win = true;
-            cout << "Jogar novamente? [Y/n]";
-            cin >> replay;
-        }
-        if(play == 9){
-            cout << "Deu velha!\n";
-            cout << "Jogar novamente? [Y/n]";
-            cin >> replay;
-        }
+            if(checkWinner(table, l-1, c-1, playersIcon[0])){
+                cout << "O jogador " << playersIcon[0] << " venceu! Parabéns!\n"; 
+                ending = true;
+            }
 
+            if(checkWinner(table, l-1, c-1, playersIcon[1])){
+                cout << "O jogador " << playersIcon[1] << " venceu! Parabéns!\n"; 
+                ending = true;
+            }
+
+            if(play == 9){
+                cout << "Deu velha!\n";
+                ending = true;
+            }
+        }while(!ending);
+        cout << "Jogar novamente? [Y/n]";
+        cin >> replay;
     }while(replay == 'y' || replay == 'Y');
     return 0;
 }
